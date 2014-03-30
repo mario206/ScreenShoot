c3a9f494a7e22a66c6caa1c55f8b812ddd7068e2
hellogdut@yeah.net <feiyuewu>
2014-03-30 18:50:02 +0800

    1.1
    添加注释

--- screenshoot.cpp
+++ screenshoot.cpp
@@ -1,19 +1,21 @@
 #include <screenshoot.h>
 ScreenShoot::ScreenShoot(QWidget *parent)
 {
-    //初始化窗体
+    // 获取屏幕尺寸
     QDesktopWidget* desktopWidget = QApplication::desktop();
     QRect deskRect = desktopWidget->screenGeometry();
+    // 将窗体设置为屏幕尺寸，去掉标题栏
     this ->resize(deskRect.width(),deskRect.height());
     this ->setWindowFlags(Qt::FramelessWindowHint);
-    this ->setWindowOpacity(0.2);
-    // 设置背景
+    // 调用setbackground() 设置背景
     setbackground(deskRect.width(),deskRect.height());
 
     //初始化变量
     g_rubber = NULL;
+
     origin = end = QPoint(0,0);
     label  = new QLabel("");
+
     label->setWindowFlags(Qt::FramelessWindowHint);
 
 }
@@ -24,10 +26,14 @@ ScreenShoot::~ScreenShoot()
 void ScreenShoot::mousePressEvent(QMouseEvent *e)
 {
     if(!g_rubber)
+    {
         g_rubber = new QRubberBand(QRubberBand::Rectangle,this);
-
+    }
+    // 将橡皮框设置课件
     g_rubber->show();
+    // 记录点击位置
     origin = e->pos();
+    // 设置橡皮框选区
     g_rubber->setGeometry(origin.x(),origin.y(),0,0);
 
 }
@@ -35,12 +41,17 @@ void ScreenShoot::mouseMoveEvent(QMouseEvent *e)
 {
     if(g_rubber)
     {
+        // 记录终点
         end = e->pos();
+
+        //调整要显示的区域
         int w = abs(end.x() - origin.x());
         int h = abs(end.y() - origin.y());
         int x = origin.x() < end.x() ? origin.x() : end.x();
         int y = origin.y() < end.y() ? origin.y() : end.y();
+
         g_rubber->setGeometry(x,y,w,h);
+        // 动态显示选区大小
         setLabel();
     }
 }
@@ -48,28 +59,34 @@ void ScreenShoot::mouseReleaseEvent(QMouseEvent *e)
 {
     if(g_rubber)
     {
+        // 记录终点
         end = e ->pos();
         g_rubber->hide();
+        // 截屏
         grabScreen();
-        origin = end = QPoint(0,0);
+        // 截屏后关闭程序和标签栏
         this ->close();
         label->close();
     }
 }
 void ScreenShoot::grabScreen()
 {
+    // 调整要截取的区域
     int wid = abs(origin.x() - end.x());
     int height = abs(origin.y() - end.y());
     int x = origin.x() < end.x() ? origin.x() : end.x();
     int y = origin.y() < end.y() ? origin.y() : end.y();
+
     //构造文件名
     QDateTime time = QDateTime::currentDateTime();
     QString str = time.toString("yyyyMMddhhmmss");
     QString picName = str + ".bmp";
-    // 截图
+
+    // 对全屏背景进行截图
     QImage pic = bg->copy(x,y,wid,height);
     pic.save(picName);
-    //打开图片
+
+    // 获取程序当前路径，打片截图
     QString path;
     QDir dir;
     path=dir.currentPath();
@@ -79,14 +96,16 @@ void ScreenShoot::grabScreen()
 }
 void ScreenShoot::setLabel()
 {
+    // 调整标签显示位置
     int wid = abs(origin.x() - end.x());
     int height = abs(origin.y() - end.y());
     int x = origin.x() < end.x() ? origin.x() : end.x();
     int y = origin.y() < end.y() ? origin.y() : end.y();
 
+    // 设置标签显示内容
     QString str = QString(" %1 x %2      ").arg(wid).arg(height);
     label->setText(str);
-
+    // 设置标签位置
     QRect rect(label->contentsRect());
     label->move(QPoint(x,y - rect.height()));
     label->show();
@@ -94,6 +113,7 @@ void ScreenShoot::setLabel()
 }
 void ScreenShoot::keyPressEvent(QKeyEvent *e)
 {
+    // 如果按下ESC键，关闭程序
     if(e->key() == Qt::Key_Escape)
     {
         this ->close();
@@ -105,7 +125,8 @@ void ScreenShoot::setbackground(int width,int height)
     //截取当前屏幕
     QScreen *screen = QGuiApplication::primaryScreen();
     screen->grabWindow(0).save("bg.bmp","bmp");
-    //读取背景图
+
+    //读取背景图，等比例缩放RGB值，使背景变暗，进入截图模式
     int red,green,blue;
 
     bg = new QImage("bg.bmp");
@@ -114,17 +135,15 @@ void ScreenShoot::setbackground(int width,int height)
     {
         for(int y = 0; y < height; y++)
         {
-            red = qRed(bg->pixel(x,y)) * 0.8 ;
-            green = qGreen(bg->pixel(x,y)) * 0.8;
-            blue = qBlue(bg->pixel(x,y)) * 0.8 ;
+            red = qRed(bg->pixel(x,y)) * 0.6 ;
+            green = qGreen(bg->pixel(x,y)) * 0.6;
+            blue = qBlue(bg->pixel(x,y)) * 0.6;
             bg_blend.setPixel( x, y, qRgb( red, green, blue ) );
         }
     }
-
-
+    // 将图片设置为背景
     QPalette   palette;
     palette.setBrush(this->backgroundRole(),QBrush(bg_blend));
     this->setPalette(palette);
-
 
 }
--- screenshoot.h
+++ screenshoot.h
@@ -2,21 +2,20 @@
 #define SCREENSHOOT_H
 
 #include <QWidget>
-#include <QRubberBand>
-#include <QLabel>
-#include <QDateTime>
-#include <QScreen>
-#include <QDesktopWidget>
+#include <QRubberBand>    // 用于创建一个橡皮筋选框
+#include <QLabel>         // 动态显示截图区域尺寸
+#include <QDateTime>      // 用当前系统时间动态生成截图的文件名
+#include <QScreen>        // 用来截取全屏
+#include <QDesktopWidget> // 用于获取屏幕尺寸
 #include <QApplication>
-#include <QPixmap>
-#include <QPalette>
-#include <QMouseEvent>
-#include <QString>
-#include <windows.h>
-#include <QImage>
-#include <QDir>
-#include <QDesktopServices>
-class MyWinEventFilter;
+#include <QPalette>       // 对话框的调色板，用于将图片设置为窗体背景
+#include <QMouseEvent>    // 重载用于获取鼠标事件
+#include <QString>        // 配合上面的<QDateTime> 用于存储文件名
+#include <windows.h>      // 调用win32API
+#include <QImage>         // 用于储存图片对象
+#include <QDir>           // 用于获取当前程序目录，得到截图的路径
+#include <QDesktopServices>  //配合 <QDir>  用系统浏览器打开图片
+
 class ScreenShoot : public QWidget
 {
     Q_OBJECT
@@ -24,20 +23,20 @@ class ScreenShoot : public QWidget
 public:
     explicit ScreenShoot(QWidget *parent = 0);
     ~ScreenShoot();
-    void mouseMoveEvent(QMouseEvent *e);
-    void mousePressEvent(QMouseEvent *e);
-    void mouseReleaseEvent(QMouseEvent *e);
-    void setbackground(int width,int height);
-    void grabScreen();
-    void setLabel();
-    void keyPressEvent(QKeyEvent *e);
+    void mouseMoveEvent(QMouseEvent *e);      // 处理鼠标移动
+    void mousePressEvent(QMouseEvent *e);     // 处理鼠标按下
+    void mouseReleaseEvent(QMouseEvent *e);   // 处理鼠标释放
+    void setbackground(int width,int height); // 初始化程序背景
+    void grabScreen();                        // 根据选取截屏，保存并打开图片
+    void setLabel();                          // 动态显示截图区域尺寸
+    void keyPressEvent(QKeyEvent *e);         // 处理esc取消按键
 private:
     QRubberBand* g_rubber;
 
-    QPoint origin;
-    QPoint end;
-    QLabel* label;
-    QImage* bg;
+    QPoint origin;                            // 记录鼠标按下坐标
+    QPoint end;                               // 记录鼠标释放坐标
+    QLabel* label;                            // 显示截图区域尺寸
+    QImage* bg;                               // 记录全屏图片
 
 
 };
